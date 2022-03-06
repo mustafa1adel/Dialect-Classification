9f3d75d634a3f58cc7f55ebb6bce106c7f33c1e7
Mustafa <mustafa1adel@gmail.com>
2022-03-06 22:38:16 +0200

    helpers functions

---
+++ helper.py
@@ -0,0 +1,21 @@
+import re
+
+def clean_text(text):
+    special_ch = re.compile('[-\+!~@#$%^&*()={}\[\]:;<.>?/\'"]')
+    english_l = re.compile('[a-zA-Z]')
+    new_lines = re.compile('\s+')
+    numbers = re.compile('[0-9]')
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+
+    text = special_ch.sub("", text)
+    text = english_l.sub("", text)
+    text = new_lines.sub(" ", text)
+    text = numbers.sub("", text)
+    text = emoji_pattern.sub("", text)
+
+    return text
