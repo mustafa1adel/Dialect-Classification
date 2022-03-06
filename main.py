34c45231af0ae1890d1fcfd065346fc273f13a88
Mustafa <mustafa1adel@gmail.com>
2022-03-06 21:38:35 +0200

    main.py
    error handeled

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pickle
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from helper import clean_text
+import json
 
 # load the model
 with open('Models/mnb_model.pkl', 'rb')as f:
@@ -11,13 +12,19 @@ app = Flask(__name__)
 @app.route('/predict/', methods=['POST', 'GET'])
 def predict() -> str:
 
-    try:
-        response = request.get_json()
-        if isinstance(response, list):
-            return jsonify({text: model.predict([clean_text(text)])[0] for text in response})
-        else:
-            return jsonify("Make sure you're sending a sequence (list/tuple) of strings")
-    except:
-        return "Make sure you're sending the request with the Content-type: application/json header."
+    response = request.get_json()
+    if isinstance(response, (list, tuple)):
+        result = dict()
+        for text in response:
+            if len(clean_text(text)) > 1 :
+                result[text] = model.predict([clean_text(text)])[0]
+            else:
+                result[text] = 'Not an Arabic word'
+        return json.dumps(result)
+    elif response == None:
+        return json.dumps("Make sure you're not sending None and sending the request with the Content-type: application/json header.")
+
+    return json.dumps("Make sure you're sending a sequence(list/tuple) of strings and sending ")
+
 
 app.run(debug=True, port=8080) #run app on port 8080 in debug mode
